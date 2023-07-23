a8969bc3328e8c2e6be701b89990fd64628c96c3
zohir kioukiou <kiouzohir@gmail.com>
2023-07-23 20:14:53 +0100

    completed assginment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+function multiply(x,z) {
+return x*z
+}
+console.log(2*31)
 
-// Write your code here
+function random(min,max){
+return Math.random();
+}
+console.log(random(0,5))
+
+function remainder (num3,num4){
+return num3 % num4
+}
+console.log(14%5)
+
+
+let b = Math.max(10,5,6,20,12,9)
+console.log(b)
